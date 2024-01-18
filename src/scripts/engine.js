ae7144dc9a53773c51f4780e4b94b3837b6ae653
Gabriel_Sales <gabrielsalesj7@gmail.com>
2024-01-17 22:12:05 -0300

    Com Controle de volume

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,5 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
 
 let mapedKeys = [];
 let audio = new Audio("src/audios/a.wav");
@@ -26,3 +27,10 @@ document.addEventListener("keydown", (e) => {
     playTune(e.key);
   }
 });
+
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+  console.log(e.target.value);
+};
+
+volumeSlider.addEventListener("input", handleVolume);
